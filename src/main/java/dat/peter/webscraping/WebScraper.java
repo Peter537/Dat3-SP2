b0489c8bacd9d93d36766d2bec1b35f69baeff1d
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-09-25 13:21:09 +0200

    Update WebScraper.java

--- src/main/java/dat/peter/webscraping/WebScraper.java
+++ src/main/java/dat/peter/webscraping/WebScraper.java
@@ -1,35 +1,135 @@
 package dat.peter.webscraping;
 
+import dat.peter.model.App_Type;
+import dat.peter.model.Game;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebScraper {
 
-    private static final String BASE_URL = "https://steamdb.info/";
-    private static final String APP_CHART_URL = BASE_URL + "app/#/charts/";
+    private static final String BASE_PATH = "SteamDB/steamdb_info.html";
+    private static final String APP_FILE_PATH = "SteamDB/#.html";
 
     public static void main(String[] args) {
-        System.out.println(getData());
+        getData().forEach(System.out::println);
     }
 
-    public static String getData() {
-        Document document = getDocument(BASE_URL);
-        System.out.println(document);
+    public static List<Game> getData() {
+        List<Game> games = new ArrayList<>();
+        Document document = getDocument(BASE_PATH);
+//        System.out.println(document);
+        document.select(".row > .span6 > .table-products.table-hover ").forEach(table -> {
+            table.select("thead > tr > th > a").forEach(thead -> {
+//                System.out.println(thead.text());
+                if (thead.text().equalsIgnoreCase("Most Played Games")) {
+                    table.select("tbody > tr").forEach(row -> {
+//                        System.out.println("========");
+                        Elements td = row.select("td");
+                        String appId = td.get(0).select("a").attr("href").split("/")[2];
+                        String name = td.get(1).text();
+                        String playersNowString = td.get(2).text();
+                        String peakTodayString = td.get(3).text();
+                        Game game = new Game();
+                        game.setApp_id(Long.parseLong(appId));
+                        game.setTitle(name);
+                        game.setAll_time_peak(Long.parseLong(peakTodayString.replace(",", "")));
+//                        System.out.println(game);
+//                        if (appId.equalsIgnoreCase("730")) {
+                        addAdditionalGameData(game, playersNowString);
+//                        }
+                        games.add(game);
+                    });
+                }
+            });
+        });
 
-        return "Hello world!";
+        return games;
     }
 
-    private static Document getDocument(String url) {
+    private static void addAdditionalGameData(Game game, String playersNowString) {
+        Document document = getDocument(APP_FILE_PATH.replace("#", String.valueOf(game.getApp_id())));
+        document.select(".header-wrapper > .container > .row.app-row > .span8 > .table.table-bordered.table-hover.table-responsive-flex > tbody > tr").forEach(e -> {
+            Elements elements = e.select("td");
+            String key = elements.get(0).text();
+            String value = elements.get(1).text();
+            if (key.equalsIgnoreCase("Release Date")) {
+                String a = elements.select("i > time").attr("datetime");
+                LocalDateTime releaseDate = LocalDateTime.parse(a,
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+//                System.out.println(releaseDate);
+                game.setRelease_date(releaseDate);
+            }
+
+            if (key.equalsIgnoreCase("Last Record Update")) {
+                String a = elements.select("i > time").attr("datetime");
+                LocalDateTime lastRecordUpdate = LocalDateTime.parse(a,
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+//                System.out.println(lastRecordUpdate);
+                game.setLast_record_update(lastRecordUpdate);
+            }
+
+//            System.out.println("Key: " + key);
+//            System.out.println("Value: " + value);
+            switch (key.toLowerCase()) {
+                case "app type":
+//                    System.out.println("App Type: " + value);
+                    game.setType(value);
+                    break;
+                case "developer":
+//                    System.out.println("Developer: " + value);
+                    String[] developers = value.split(", ");
+                    for (String developer : developers) {
+//                        System.out.println(developer);
+                        game.addDeveloper(developer);
+                    }
+                    break;
+                case "publisher":
+//                    System.out.println("Publisher: " + value);
+                    String[] publishers = value.split(", ");
+                    for (String publisher : publishers) {
+//                        System.out.println(publisher);
+                        game.addPublisher(publisher);
+                    }
+                    break;
+                case "supported systems":
+//                    System.out.println("Supported Systems: " + value);
+                    String[] systems = value.split(" ");
+                    for (String system : systems) {
+//                        System.out.println(system);
+                        game.addSystem(system);
+                    }
+                    break;
+                case "last change number":
+                    game.setLast_change_number(Long.parseLong(value));
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
+    private static Document getDocument(String path) {
         try {
-            Document document = Jsoup.connect(url)
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-                            "(KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
-                    .get();
-            Thread.sleep(2000);
-            return document;
-        } catch (IOException | InterruptedException e) {
+            List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            return Jsoup.parse(String.join("\n", lines));
+
+//            Document document = Jsoup.connect(url)
+//                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+//                            "(KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+//                    .get();
+//            Thread.sleep(2000);
+//            return document;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
