d736f32d13c1150a46c015c1dc3024f068667043
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-09-25 11:44:07 +0200

    Update WebScraper.java

--- src/main/java/dat/peter/webscraping/WebScraper.java
+++ src/main/java/dat/peter/webscraping/WebScraper.java
@@ -23,7 +23,10 @@ public class WebScraper {
 
     private static Document getDocument(String url) {
         try {
-            Document document = Jsoup.connect(url).get();
+            Document document = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                            "(KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+                    .get();
             Thread.sleep(2000);
             return document;
         } catch (IOException | InterruptedException e) {
