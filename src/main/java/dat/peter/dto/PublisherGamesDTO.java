5071352b19134277e406e6211d011d79abca858b
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-09-25 14:25:47 +0200

    deleted dtos

--- src/main/java/dat/peter/dto/PublisherGamesDTO.java
+++
@@ -1,8 +0,0 @@
-package dat.peter.dto;
-
-import dat.peter.model.Game;
-import dat.peter.model.Publisher;
-
-import java.util.Set;
-
-public record PublisherGamesDTO(Publisher publisher, Set<Game> games) { }
