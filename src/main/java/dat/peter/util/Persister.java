3fd1aeb67ccef6c6109cc45f93719de3298952ad
MagKon <magnus.kongstad@gmail.com>
2023-09-25 14:26:10 +0200

    Added persistnews

--- src/main/java/dat/peter/util/Persister.java
+++ src/main/java/dat/peter/util/Persister.java
@@ -7,29 +7,46 @@ import dat.peter.model.News;
 import dat.peter.util.api.SteamAPI;
 import dat.peter.util.api.SteamCall;
 import dat.peter.util.webscraping.WebScraper;
+import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 public class Persister {
-    private DAO<News> newsDAO = new DAO<>(News.class, HibernateConfig.getEntityManagerFactoryConfig("SteamDB"));
-    private DAO<Game> gameDAO = new DAO<>(Game.class, HibernateConfig.getEntityManagerFactoryConfig("SteamDB"));
+    private final DAO<News> newsDAO = new DAO<>(News.class, HibernateConfig.getEntityManagerFactoryConfig("SteamDB"));
+    private final DAO<Game> gameDAO = new DAO<>(Game.class, HibernateConfig.getEntityManagerFactoryConfig("SteamDB"));
     public void persist() {
         List<Game> games = WebScraper.getData();
         System.out.println(games.size());
 
         for (Game game : games) {
             game = gameDAO.update(game);
-            SteamCall sc = SteamAPI.call(game.getApp_id());
-            if (sc != null) {
-                News news = newsDAO.findById(sc.getGid());
-                if (news != null) {
-                    System.out.println("exists");
-                } else {
-                    System.out.println("not exists");
-                    News newss = new News(sc.getGid(), sc.getTitle(), sc.getUrl(), sc.getAuthor(), sc.getContents(), sc.getFeed_label(), sc.getDateAsLocalDateTime(), sc.getFeed_name(), sc.getFeed_type(), game);
-                    newsDAO.update(newss);
-                }
-            }
+
+            game = persistNews(game);
         }
+    }
+
+    private Game persistNews(Game game) {
+        // Get news from Steam API
+        SteamCall sc = SteamAPI.call(game.getApp_id());
+
+        if (sc != null) {
+            News news = newsDAO.findById(sc.getGid());
+            if (news != null) {
+                System.out.println("exists");
+            } else {
+                System.out.println("not exists");
+                // Create new object
+                news = new News(sc.getGid(), sc.getTitle(), sc.getUrl(), sc.getAuthor(), sc.getContents(), sc.getFeed_label(), sc.getDateAsLocalDateTime(), sc.getFeed_name(), sc.getFeed_type(), game);
+
+                // Persist object and return attached object
+                news = newsDAO.update(news);
+
+                // Add news to game
+                game.getNews().add(news);
+            }
+
+            gameDAO.update(game);
+        }
+        return game;
     }
 }
