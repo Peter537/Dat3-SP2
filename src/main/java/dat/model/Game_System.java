3b57a304ed5582054997922ed3f08a5c84255abb
MagKon <magnus.kongstad@gmail.com>
2023-09-26 10:35:17 +0200

    Added constructor, cascade and Generated

--- src/main/java/dat/model/Game_System.java
+++ src/main/java/dat/model/Game_System.java
@@ -1,20 +1,23 @@
 package dat.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 public class Game_System {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne
     private Game fk_app_id;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.MERGE)
     private System platform;
 
-
+    public Game_System(Game fk_app_id, System platform) {
+        this.fk_app_id = fk_app_id;
+        this.platform = platform;
+    }
 }
